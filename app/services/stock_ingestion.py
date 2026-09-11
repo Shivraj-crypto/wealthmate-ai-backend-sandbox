249ad16c952597bbcd35f63320f69358172baa4c
Shivraj.T <78204939+Shivraj-crypto@users.noreply.github.com>
2026-09-12 01:29:12 +0530

    r change

--- app/services/stock_ingestion.py
+++ app/services/stock_ingestion.py
@@ -2,5 +2,8 @@ from app.services.yahoo_service import get_formatted_quotes
 
 quotes = get_formatted_quotes(["AAPL", "MSFT", "GOOG"])
 
+q = []
 for quote in quotes:
-    print(quote)
+    q.append(quote)
+
+print(q)
