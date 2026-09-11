efe79c18e97edc6f543573d0b545efc117f2f6f0
Shivraj.T <78204939+Shivraj-crypto@users.noreply.github.com>
2026-09-12 01:44:15 +0530

    feat: refactor stock ingestion to include database dependency injection and streamline quote retrieval

--- app/services/stock_ingestion.py
+++ app/services/stock_ingestion.py
@@ -1,9 +1,33 @@
 from app.services.yahoo_service import get_formatted_quotes
+from ..database import SessionLocal
+from ..models import Stock
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException, status, Path
+from ..schemas import *
 
-quotes = get_formatted_quotes(["AAPL", "MSFT", "GOOG"])
+def get_db():
+    db = SessionLocal()
 
-q = []
-for quote in quotes:
-    q.append(quote)
+    try:
+        yield db
+    
+    finally:
+        db.close()
 
-print(q)
+db_dependency_injection = Annotated[Session, Depends(get_db)]
+
+quotes = get_formatted_quotes(["AAPL"])
+
+def add_to_db(db :db_dependency_injection):
+    data = Stock(
+        symbol=quotes[0]["symbol"],
+        name=quotes[0]["shortName"],
+        currency=quotes[0]["currency"],
+        market_state=quotes[0]["marketState"]
+    )
+
+    db.add(data)
+    db.commit()
+    db.refresh(data)
+
