e4d7127b399a5b63f06ca4d4956b8b0ef379e6d9
Shivraj.T <78204939+Shivraj-crypto@users.noreply.github.com>
2026-09-16 23:17:47 +0530

    feat: add response model to stock price retrieval endpoint and improve error handling

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from typing import Annotated
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
 from .models import Stock, StockPrice
+from .schemas import StockPriceResponse
 
 
 router = APIRouter()
@@ -25,14 +26,22 @@ class StockTicker(BaseModel):
 db_dependency_injection = Annotated[Session, Depends(get_db)]
 
 
-@router.get("/stocks_price", status_code=status.HTTP_200_OK)
-async def get_current_stock_price(db : db_dependency_injection, input : StockTicker):
+@router.get("/stocks_price", status_code=status.HTTP_200_OK, response_model=StockPriceResponse)
+
+async def get_current_stock_price(db: db_dependency_injection, input: StockTicker):
+
     stock = db.query(Stock).filter(Stock.symbol == input.symbol.upper()).first()
 
     if stock is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="NO DATA")
 
     stock_data = db.query(StockPrice).filter(stock.id == StockPrice.stock_id).order_by(StockPrice.recorded_at.desc()).first()
+
+    if stock_data is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="NO PRICE DATA"
+        )
 
     return stock_data
 
