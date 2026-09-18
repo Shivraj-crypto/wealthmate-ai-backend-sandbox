772a4029bbb633de5ea17be784f9e7cab3708f5d
Shivraj.T <78204939+Shivraj-crypto@users.noreply.github.com>
2026-09-18 22:40:33 +0530

    refactor: update stock price retrieval to return Stock model instead of StockPrice

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from .database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
-from .models import Stock, StockPrice
+from .models import Stock, HistoricalPrice
 from .schemas import StockPriceResponse
 
 
@@ -34,12 +34,7 @@ async def get_current_stock_price(db: db_dependency_injection, symbol: str):
     if stock is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="NO DATA")
 
-    stock_data = db.query(StockPrice).filter(stock.id == StockPrice.stock_id).order_by(StockPrice.recorded_at.desc()).first()
-
-    if stock_data is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="NO PRICE DATA")
-
-    return stock_data
+    return stock
 
 
     
